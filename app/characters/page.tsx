4234027984d49581b86544bd1b609623001b9ae1
NightangleOvah <Isiralegacy@gmail.com>
2026-08-29 15:22:10 +0530

    feat: add character engine route

---
+++ app/characters/page.tsx
@@ -0,0 +1,5 @@
+import { CharacterEngine } from '@/components/CharacterEngine'
+
+export default function CharactersPage(){
+  return <main className="min-h-screen bg-[#0f021e] py-24"><CharacterEngine/></main>
+}
